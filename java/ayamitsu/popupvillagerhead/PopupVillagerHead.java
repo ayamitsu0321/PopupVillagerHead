a4409b11885ce1b000d265cd127a293b8da8f631
ayamitsu0321 <ayamitsu0321@yahoo.co.jp>
2015-08-16 11:13:43 +0900

    change entity
    Head to Spider

--- java/ayamitsu/popupvillagerhead/PopupVillagerHead.java
+++ java/ayamitsu/popupvillagerhead/PopupVillagerHead.java
@@ -13,13 +13,17 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 @Mod(
-        modid = "ayamitsu0321.popupvillagerhead",
-        name = "PopupVillagerHead",
-        version = "1.0.0"
+        modid = PopupVillagerHead.MODID,
+        name = PopupVillagerHead.NAME,
+        version = PopupVillagerHead.VERSION
 )
 public class PopupVillagerHead {
 
-    @Mod.Instance("ayamitsu.popupvillagerhead")
+    public static final String MODID = "popupvillagerhead";
+    public static final String NAME = "PopupVillagerHead";
+    public static final String VERSION = "1.1.0";
+
+    @Mod.Instance(PopupVillagerHead.MODID)
     public static PopupVillagerHead instance;
 
     @SidedProxy(clientSide = "ayamitsu.popupvillagerhead.client.ClientProxy", serverSide = "ayamitsu.popupvillagerhead.server.ServerProxy")
